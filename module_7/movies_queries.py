e48c316ce5bd266bebe8c9b14e919daf60dd9114
stewart-ronald <72624044+stewart-ronald@users.noreply.github.com>
2022-11-28 19:24:02 -0500

    Update movies_queries.py

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -1,41 +1,61 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(
-    user='movies_user',
-    password='popcorn',
-    host='127.0.0.1',
-    database='movies',
-    raise_on_warnings=True
-)
-mycursor = mydb.cursor()
+config = {
+    "user": "movies_user",
+    "password": "popcorn",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
 
-# displaying all studio fields
-mycursor.execute("SELECT * FROM studio")
+try:
+    db = mysql.connector.connect(**config)
 
-myresult = mycursor.fetchall()
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
 
-for x in myresult:
-    print(x)
-# displaying all genre fields
-mycursor.execute("SELECT * FROM genre")
+    print("\n--DISPLAYING Studio RECORDS--\n")
 
-myresult = mycursor.fetchall()
+    cursor = db.cursor()
 
-for x in myresult:
-    print(x)
+    # The first query is to select all the fields for the studio table
+    cursor.execute("SELECT studio_id, studio_name FROM movies.studio")
+    studios = cursor.fetchall()
+    for studio in studios:
+        print(f"Studio ID:    {studio[0]}");
+        print(f"Studio Name:  {studio[1]}\n");
 
-# displaying films less than 2 hours
-mycursor.execute("SELECT film_name FROM film WHERE film_runtime < 120")
+    # The second query is to select all the fields for the genre table
+    cursor.execute("SELECT genre_id, genre_name FROM movies.genre")
+    studios = cursor.fetchall()
+    for studio in studios:
+        print(f"Genre ID:    {studio[0]}");
+        print(f"Genre Name:  {studio[1]}\n");
 
-myresult = mycursor.fetchall()
+    # The third query is to select the movie names for those movies that have a run time of less than two hours.
+    cursor.execute("SELECT film_id, film_name FROM movies.film WHERE film_runtime <120")
+    studios = cursor.fetchall()
+    for studio in studios:
+        print(f"Film ID:    {studio[0]}");
+        print(f"Film Name:  {studio[1]}\n");
 
-for x in myresult:
-    print(x)
+    # The fourth query is to get a list of film names, and directors grouped by director.
+    cursor.execute("SELECT film_id, film_name, film_director FROM movies.film")
+    studios = cursor.fetchall()
+    for studio in studios:
+        print(f"Film ID:        {studio[0]}");
+        print(f"Film Name:      {studio[1]}");
+        print(f"Film Director:  {studio[2]}\n");
 
-# displaying film names and directors grouped by director
-mycursor.execute("SELECT film_name, film_director FROM film GROUP BY film_director, film_name")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
 
-myresult = mycursor.fetchall()
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
 
-for x in myresult:
-    print(x)
+    else:
+        print(err)
+
+finally:
+    db.close()
